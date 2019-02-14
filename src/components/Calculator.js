d4c9641fe73e8bbe16bb453d513bcd32d6236242
Gareth Cassin <nissacjg@gmail.com>
2019-02-14 08:44:04 +0000

    get btns showing

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import Display from './Display'
+import Inputs from './Inputs'
+import Button from './Button'
 
 class Calculator extends Component {
   constructor (props) {
@@ -9,13 +12,18 @@ class Calculator extends Component {
     }
   }
   render() {
+    let buttonArr = [{gridArea: 'ac', label: 'AC'}, {gridArea: 'plusMinus', label: '+/-'}]
+    let newBtns = buttonArr.map(button => <Button gridArea={button.gridArea} label={button.label}/>)
     return (
       <div className='container'>
+        < Display className='display'/>
         {/* Display value={this.state.displayFigure} */}
         {/* Inputs */}
               {/* Buttons - prop with value, prop if number or math*/}
-        <div className='display'>01234567890</div>
+        {/* <div className='display'>01234567890</div> */}
         <div className='btn ac'>AC</div>
+        {/* <Inputs /> */}
+        {newBtns}
       </div>
     )
   }
