c4c5e36fa7d8d59e117b054a3c2a9f5dc029c331
Gareth Cassin <nissacjg@gmail.com>
2019-02-14 08:43:46 +0000

    display component

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Display extends Component {
+  render() {
+    return (
+      <div className='display'>
+        Show Display Here
+      </div>
+    )
+  }
+}
+
+export default Display
