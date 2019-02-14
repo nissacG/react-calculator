863c0b61a65448eb24311510fae29959bd893e92
Gareth <nissacjg@gmail.com>
2019-02-14 09:17:42 +0000

    updates data source for btns

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,8 +12,28 @@ class Calculator extends Component {
     }
   }
   render() {
-    let buttonArr = [{gridArea: 'ac', label: 'AC'}, {gridArea: 'plusMinus', label: '+/-'}]
-    let newBtns = buttonArr.map(button => <Button gridArea={button.gridArea} label={button.label}/>)
+    let buttonArr = [
+      {gridArea: 'ac', label: 'AC'},
+      {gridArea: 'plusMinus', label: '+/-'},
+      {gridArea: 'nine', label: '9'},
+      {gridArea: 'percent', label: '%'},
+      {gridArea: 'divide', label: '÷'},
+      {gridArea: 'multiply', label: '×'},
+      {gridArea: 'minus', label: '-'},
+      {gridArea: 'plus', label: '+'},
+      {gridArea: 'equal', label: '='},
+      {gridArea: 'decimal', label: '.'},
+      {gridArea: 'zero', label: '0'},
+      {gridArea: 'one', label: '1'},
+      {gridArea: 'two', label: '2'},
+      {gridArea: 'three', label: '3'},
+      {gridArea: 'four', label: '4'},
+      {gridArea: 'five', label: '5'},
+      {gridArea: 'six', label: '6'},
+      {gridArea: 'seven', label: '7'},
+      {gridArea: 'eight', label: '8'},
+    ]
+    let newBtns = buttonArr.map(button => <Button key={button.gridArea} gridArea={button.gridArea} label={button.label}/>)
     return (
       <div className='container'>
         < Display className='display'/>
@@ -21,7 +41,7 @@ class Calculator extends Component {
         {/* Inputs */}
               {/* Buttons - prop with value, prop if number or math*/}
         {/* <div className='display'>01234567890</div> */}
-        <div className='btn ac'>AC</div>
+        {/* <div className='btn ac'>AC</div> */}
         {/* <Inputs /> */}
         {newBtns}
       </div>
