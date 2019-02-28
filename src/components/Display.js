f7436fd593932861c4262f96a79c8310f766daeb
Gareth Cassin <nissacjg@gmail.com>
2019-02-28 08:56:23 +0000

    pass class as prop

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Display = ({ display }) => (
-  <div className="display">
+const Display = ({ display, clsName }) => (
+  <div className={clsName}>
     {display}
   </div>
 )
